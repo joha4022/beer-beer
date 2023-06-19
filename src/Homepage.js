90b1c9c7a9c19c311563021e75e239647cc42589
juju <joha4022@gmail.com>
2023-06-18 21:30:32 -0700

    initial commit

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+export default function Homepage() {
+    return (
+        <>
+            <h1>Beer Beer</h1>
+            <div className="menu-bar">
+                <span>Micro Breweries</span>
+                <span>Brew Pubs</span>
+                <span>Large Pubs</span>
+                
+            </div>
+        </>
+    )
+}
