0a0c39e56c12a928791bb8df0d5e2acb6e53eab1
Joo Young Han <joha4022@gmail.com>
2023-06-21 11:07:45 -0700

    Create Breweries.js

---
+++ src/Breweries.js
@@ -0,0 +1,10 @@
+import { AppContext } from './App';
+import { useContext } from 'react';
+
+export default function Breweries() {
+  return(
+    <>
+    this page should return search reults
+    </>
+  )
+}
