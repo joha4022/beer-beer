59f3575c382bca0adb2b642d97e692ccb6e4b3c5
joha4022 <joha4022@gmail.com>
2023-06-24 05:04:30 +0900

    added gif for initial loader

--- src/Loading.js
+++ src/Loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
     return(
         <div className="loading">
-            <img src="../images/beer (1).png"></img>
+            <img src="../images/1Cc6.gif"></img>
             <h2 className="brewingh2">Brewing...</h2>
         </div>
     )
